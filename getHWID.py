b90406a5c25b203869a8ff0c2b09c21593b23fd2
rthennan <rajesh.thennan@gmail.com>
2023-08-03 09:35:27 +0530

    Update getHWID.py

--- getHWID.py
+++ getHWID.py
@@ -1,4 +1,10 @@
-import os
+import subprocess
 
-uid = os.getuid()
-print(f"User ID: {uid}")
+def get_mac_disk_serial_number():
+    cmd = "/usr/sbin/diskutil info / | grep 'Serial Number (system)' | awk '{print $NF}'"
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True, check=True)
+    serial_number = result.stdout.strip()
+    return serial_number
+
+mac_disk_serial_number = get_mac_disk_serial_number()
+print(f"macOS Disk Serial Number: {mac_disk_serial_number}")
