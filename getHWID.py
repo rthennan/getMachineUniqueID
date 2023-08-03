7e393161ca3e8f9799d7329f38136d969b24aacb
rthennan <rajesh.thennan@gmail.com>
2023-08-03 09:57:19 +0530

    Update getHWID.py

--- getHWID.py
+++ getHWID.py
@@ -1,4 +1,9 @@
 import subprocess
+import platform
+
+osName = platform.system()
+
+print('OS Name',osName)
 
 def get_mac_hardware_uuid():
     cmd = "/usr/sbin/ioreg -rd1 -c IOPlatformExpertDevice | grep 'IOPlatformUUID' | awk '{print $NF}'"
@@ -6,5 +11,16 @@ def get_mac_hardware_uuid():
     hardware_uuid = result.stdout.decode().strip()
     return hardware_uuid
 
-mac_hardware_uuid = get_mac_hardware_uuid()
-print(f"macOS Hardware UUID: {mac_hardware_uuid}")
+def get_disk_serial_number():
+    with open('/proc/self/mounts', 'r') as f:
+        root_device = next(line.split()[0] for line in f if line.startswith('/dev/'))
+
+    with open('/sys/block/{}/serial'.format(root_device.split('/')[-1]), 'r') as f:
+        return f.read().strip()
+
+if osName=='Darwin':
+    mac_hardware_uuid = get_mac_hardware_uuid()
+    print(f"macOS Hardware UUID: {mac_hardware_uuid}")
+elif osName == 'Linux':
+    disk_serial_number = get_disk_serial_number()
+    print(f"Disk Serial Number: {disk_serial_number}")
